2aaa337d38d999b8fcfc942d452f397fed39d5af
louischien557 <louischien557@gmail.com>
2025-11-30 15:28:14 +0800

    20251130

--- 1130/line_messagingAPI.py
+++ 1130/line_messagingAPI.py
@@ -3,15 +3,17 @@ import json
 
 # ================= 設定區 =================
 # 1. 剛剛發行的 Channel Access Token (長的那串)
-TOKEN = "T3zoTOT60e7F+k6Wm0mOHy/Y7ckRJrfPgpZZjrYIGTKror7KhIVY5wNAicahyv4jX/Uz683W8n/DzYvVCDG9K0hPmpUqFuF4PQ5S/1iTSrs7Z/ZSgHYaR6Vj4YOZMWcHbJnTSmvh+w11T3ZkaOgzWAdB04t89/1O/w1cDnyilFU="
+TOKEN = "NXG4CXoQXRIaA92OsudN9HJcu61cp/ST9EU2qUIKFudU9/gAHcjQJaNfyHN+7SNJX/Uz683W8n/DzYvVCDG9K0hPmpUqFuF4PQ5S/1iTSrv5EQ65g91NyxibhsgznlLExke8jwaEQO1Dzpy9ZFrjpwdB04t89/1O/w1cDnyilFU="
 
 # 2. 你的 User ID (U開頭的那串)
 USER_ID = "U0547321a842dadf0805790b9cb6aa845"
 
 # =========================================
 
+# 傳送的訊息
 text = "嘿嘿嘿!"
 
+# https://developers.line.biz/en/reference/messaging-api/#send-push-message
 url = "https://api.line.me/v2/bot/message/push"
 headers = {
     "Content-Type": "application/json",
