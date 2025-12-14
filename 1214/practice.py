ff0bc7154acdd3175d89a3417d9e887782c0b242
Zac <a19860101@gmail.com>
2025-12-14 11:27:53 +0800

    20251214

--- 1214/practice.py
+++ 1214/practice.py
@@ -4,7 +4,7 @@ import bs4
 
 
 price = int(input('請輸入金額：'))
-is_tax = bool(input('含稅還是未稅？（含稅輸入1，未稅輸入0）'))
+is_tax = input('含稅還是未稅？（含稅輸入1，未稅輸入0）')
 tax = int(input('消費稅為多少？（10%輸入10，8%輸入8）'))
 # price = 1000
 # is_tax = True
@@ -17,23 +17,23 @@ result = res.text
 htmlfile = bs4.BeautifulSoup(result, 'html.parser')
 jpy = float(htmlfile.select_one('table tbody tr:nth-of-type(8) td:nth-of-type(3)').text)
 
-if is_tax is True:
+if is_tax == '1':
     tax_in = price
     if tax == 10:
-        tax_out = price / 1.1
+        tax_out = price / (1+tax/100)
     else:
-        tax_out = price / 1.08
+        tax_out = price / (1+tax/100)
 
 
 else:
     tax_out = price
     if tax == 10:
-        tax_in = price * 1.1
+        tax_in = price * (1+tax/100)
     else:
-        tax_out = price * 1.08
+        tax_out = price * (1+tax/100)
 
 
-print(f'含稅價日幣為{tax_in}，台幣為{tax_in * jpy}')
-print(f'未稅價日幣為{tax_out}，台幣為{tax_out * jpy}')
+print(f'含稅價日幣為{tax_in:.0f}，台幣為{(tax_in * jpy):.0f}')
+print(f'未稅價日幣為{tax_out:.0f}，台幣為{(tax_out * jpy):.0f}')
 print(f'消費稅{tax}%')
-print(f'日幣價差為{tax_in - tax_out}，台幣價差為{(tax_in - tax_out) * jpy}')
+print(f'日幣價差為{(tax_in - tax_out):.0f}，台幣價差為{((tax_in - tax_out) * jpy):.0f}')
