8986623862276a18b918b9235ba4c1858754f6dc
Zac <a19860101@gmail.com>
2025-12-14 10:26:00 +0800

    20251214

--- 1214/rate-2.py
+++ 1214/rate-2.py
@@ -13,10 +13,16 @@ driver.maximize_window()
 # getNewRate = driver.find_element(By.XPATH,'//*[@id="ie11andabove"]/div/p[1]/a[1]')
 getNewRate = driver.find_element(By.LINK_TEXT, '取得最新報價')
 
-time.sleep(5)
+time.sleep(2)
 
 getNewRate.click()
 
-time.sleep(5)
+time.sleep(2)
+jpy_c = driver.find_element(By.XPATH, '//*[@id="ie11andabove"]/div/table/tbody/tr[8]/td[1]')
+jpy = driver.find_element(By.XPATH, '//*[@id="ie11andabove"]/div/table/tbody/tr[8]/td[3]')
+usd = driver.find_element(By.XPATH, '//*[@id="ie11andabove"]/div/table/tbody/tr[1]/td[3]')
+krw = driver.find_element(By.XPATH, '//*[@id="ie11andabove"]/div/table/tbody/tr[16]/td[3]')
+
+print(jpy_c.text, jpy.text)
 
 driver.close()
