3fe2f70ce0b84f5aa19b7e7ff81bdb15fa99ff68
Zac <a19860101@gmail.com>
2025-12-14 11:10:03 +0800

    20251214

---
+++ 1214/practice.py
@@ -0,0 +1,39 @@
+import requests
+import bs4
+
+
+
+price = int(input('請輸入金額：'))
+is_tax = bool(input('含稅還是未稅？（含稅輸入1，未稅輸入0）'))
+tax = int(input('消費稅為多少？（10%輸入10，8%輸入8）'))
+# price = 1000
+# is_tax = True
+# tax = 10
+
+#取得匯率
+url = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
+res = requests.get(url)
+result = res.text
+htmlfile = bs4.BeautifulSoup(result, 'html.parser')
+jpy = float(htmlfile.select_one('table tbody tr:nth-of-type(8) td:nth-of-type(3)').text)
+
+if is_tax is True:
+    tax_in = price
+    if tax == 10:
+        tax_out = price / 1.1
+    else:
+        tax_out = price / 1.08
+
+
+else:
+    tax_out = price
+    if tax == 10:
+        tax_in = price * 1.1
+    else:
+        tax_out = price * 1.08
+
+
+print(f'含稅價日幣為{tax_in}，台幣為{tax_in * jpy}')
+print(f'未稅價日幣為{tax_out}，台幣為{tax_out * jpy}')
+print(f'消費稅{tax}%')
+print(f'日幣價差為{tax_in - tax_out}，台幣價差為{(tax_in - tax_out) * jpy}')
