1783fe77108f3060c4df2408833ccfbbbfcec9d6
Zac <a19860101@gmail.com>
2025-12-21 15:54:53 +0800

    20251221

--- 1221/sqlite-practice.py
+++ 1221/sqlite-practice.py
@@ -38,6 +38,7 @@ def get_all_users():
         print(data)
 
 def delete_user():
+    get_all_users()
     sql = 'DELETE FROM users WHERE id = ?'
     while True:
         pid = input('請輸入要刪除的id（輸入q取消）：')
@@ -52,6 +53,21 @@ def delete_user():
         else:
             print('該ID不存在')
 
+def search_users():
+    sql_search = 'SELECT * FROM users WHERE name LIKE ? OR phone LIKE ? OR email LIKE ?'
+    while True:
+        query = input('請輸入關鍵字（輸入q取消）：')
+        if query=='q':
+            break
+        cursor.execute(sql_search, [f'%{query}%', f'%{query}%', f'%{query}%'])
+
+        print(cursor.rowcount)
+        # if cursor.rowcount > 0:
+        #     datas = cursor.fetchall()
+        #     for data in datas:
+        #         print(data)
+        # else:
+        #     print('找不到啦')
 
 def main():
     check_create_table()
@@ -61,6 +77,7 @@ def main():
         print('1. 顯示所有資料')
         print('2. 新增會員')
         print('3. 移除會員')
+        print('4. 關鍵字搜尋')
         print('q. 結束程式')
         print('=' * 60)
         choice = input('請輸入項目（1,2,3,q）：')
@@ -71,6 +88,8 @@ def main():
             create_user()
         elif choice == '3':
             delete_user()
+        elif choice == '4':
+            search_users()
         elif choice == 'q':
             break
         else:
@@ -79,6 +98,8 @@ def main():
     print('='*60)
     print('滾，程式結束')
     print('='*60)
+
+    conn.close()
 
 if __name__ == '__main__':
     main()
