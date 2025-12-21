c5d88337b4b32e1e69f8915d5fe0cf37c86b80fe
Zac <a19860101@gmail.com>
2025-12-21 10:47:29 +0800

    20251221

--- 1221/sqlite-2.py
+++ 1221/sqlite-2.py
@@ -2,8 +2,13 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 cursor = conn.cursor()
 
-sql_insert = 'INSERT INTO users(name, phone, email)VALUES("john","0987654321","asdf@gmail.com")'
-cursor.execute(sql_insert)
+# 新增資料
+# INSERT INTO 資料表名稱(欄位1,欄位2,...)VALUES(值1,值2,...)
+# sql_insert = 'INSERT INTO users(name, phone, email)VALUES("john","0987654321","asdf@gmail.com")'
+# cursor.execute(sql_insert)
+
+sql_insert = 'INSERT INTO users(name, phone, email)VALUES(?,?,?)'
+cursor.execute(sql_insert,['mary', '0987545545', 'asdfasdfsadf@gmail.com'])
 
 conn.commit()
 conn.close()
