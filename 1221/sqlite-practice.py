3b44bb8866350c9228243c6c9b34e7e41df01239
Zac <a19860101@gmail.com>
2025-12-21 15:35:49 +0800

    20251221

--- 1221/sqlite-practice.py
+++ 1221/sqlite-practice.py
@@ -37,20 +37,40 @@ def get_all_users():
     for data in datas:
         print(data)
 
+def delete_user():
+    sql = 'DELETE FROM users WHERE id = ?'
+    while True:
+        pid = input('請輸入要刪除的id（輸入q取消）：')
+        if pid == 'q':
+            print('退出刪除模式')
+            break
+        cursor.execute(sql, [pid])
+        conn.commit()
+
+        if cursor.rowcount > 0:
+            print(f'ID {pid}已被刪除')
+        else:
+            print('該ID不存在')
+
+
 def main():
     check_create_table()
     while True:
         print('會員管理系統')
+        print('=' * 60)
         print('1. 顯示所有資料')
         print('2. 新增會員')
+        print('3. 移除會員')
         print('q. 結束程式')
-
-        choice = input('請輸入項目（1,2,q）')
+        print('=' * 60)
+        choice = input('請輸入項目（1,2,3,q）：')
 
         if choice == '1':
             get_all_users()
         elif choice == '2':
             create_user()
+        elif choice == '3':
+            delete_user()
         elif choice == 'q':
             break
         else:
