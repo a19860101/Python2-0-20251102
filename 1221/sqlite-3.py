cecb7b8f5a092951971c19223007104ab5c1f01a
Zac <a19860101@gmail.com>
2025-12-21 13:21:08 +0800

    20251221

--- 1221/sqlite-3.py
+++ 1221/sqlite-3.py
@@ -2,20 +2,30 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 cursor = conn.cursor()
 
-# sql_select = 'SELECT * FROM users'
+sql_select = 'SELECT * FROM users'
 # sql_select = 'SELECT name FROM users'
 # sql_select = 'SELECT name,phone FROM users'
 # sql_select = 'SELECT * FROM users WHERE name = "john"'
 # sql_select = 'SELECT * FROM users WHERE name = ?'
 # sql_select = 'SELECT * FROM users WHERE name = ? AND phone = ?'
-sql_select = 'SELECT * FROM users WHERE name = ? OR phone = ?'
+# sql_select = 'SELECT * FROM users WHERE name = ? OR phone = ?'
 
 
+# datas = cursor.execute(sql_select)
 # datas = cursor.execute(sql_select,['123','123123123'])
+# cursor.execute(sql_select)
+# datas = cursor.fetchall()
 
-sql_search = 'SELECT * FROM users WHERE name LIKE ? OR phone LIKE ?'
+# 模糊比對
+sql_search = 'SELECT * FROM users WHERE name LIKE ? OR phone LIKE ? OR email LIKE ?'
 s = 'gmail'
-datas = cursor.execute(sql_search, [f'%{s}%'])
+cursor.execute(sql_search, [f'%{s}%',f'%{s}%', f'%{s}%'])
+datas = cursor.fetchall()
+
+for data in datas:
+    print(data)
+
+print('+'*50)
 
 for data in datas:
     print(data)
