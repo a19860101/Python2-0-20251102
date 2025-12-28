30be1858a48a0ab7f00cd54984b7ac6c1c05c6c6
Zac <a19860101@gmail.com>
2025-12-28 11:03:04 +0800

    20251228

---
+++ 1228/mariadb-1.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+config = {
+    'user': 'root',
+    'password': '88888888',
+    'host': '127.0.0.1',
+    'database': 'lccnet'
+}
+
+conn = mysql.connector.connect(**config)
+cursor = conn.cursor()
+
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS users(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50),
+        phone VARCHAR(50),
+        email VARCHAR(50)
+    )
+'''
+
+cursor.execute(create_table_query)
+
+cursor.close()
+conn.close()
