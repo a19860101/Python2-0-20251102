c55f804b123be6bbc1d34096d2f69c8b3d343eec
Zac <a19860101@gmail.com>
2025-12-28 11:57:31 +0800

    20251228

--- 1228/mariadb-1.py
+++ 1228/mariadb-1.py
@@ -5,7 +5,8 @@ config = {
     'user': 'root',
     'password': '88888888',
     'host': '127.0.0.1',
-    'database': 'lccnet'
+    'database': 'lccnet',
+    'use_pure': True
 }
 
 conn = mysql.connector.connect(**config)
