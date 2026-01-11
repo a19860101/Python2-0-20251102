de42aa1ff3fa94487a03da7f40e6a39814e18afa
Zac <a19860101@gmail.com>
2026-01-11 15:57:15 +0800

    20260111

--- 0111/stock-1.py
+++ 0111/stock-1.py
@@ -1,6 +1,22 @@
 import yfinance as yf
 # pip install yfinance
 
-tsmc = yf.Ticker("0050.TW")
-hist = tsmc.history(period="1mo")
-print(hist)
+tsmc = yf.Ticker("2330.TW")
+# 查詢歷史紀錄
+# m：分鐘
+# d：日
+# mo：月
+# y：年
+# hist = tsmc.history(period="2d")
+# interval區間
+data = tsmc.history(period='3mo')
+
+# print(data['Close'])
+
+data['MA5'] = data['Close'].rolling(window=5).mean()
+data['MA30'] = data['Close'].rolling(window=30).mean()
+data['MA60'] = data['Close'].rolling(window=60).mean()
+
+print(data['MA5'])
+print(data['MA30'])
+print(data['MA60'])
