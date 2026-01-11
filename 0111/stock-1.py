5f4da2035f353b4c7bd8842b2dbfa6a46e2de457
Zac <a19860101@gmail.com>
2026-01-11 15:57:55 +0800

    20260111

--- 0111/stock-1.py
+++ 0111/stock-1.py
@@ -11,10 +11,14 @@ tsmc = yf.Ticker("2330.TW")
 # interval區間
 data = tsmc.history(period='3mo')
 
+# 收盤價
 # print(data['Close'])
 
+# 5日均線
 data['MA5'] = data['Close'].rolling(window=5).mean()
+# 30日均線
 data['MA30'] = data['Close'].rolling(window=30).mean()
+# 60日均線
 data['MA60'] = data['Close'].rolling(window=60).mean()
 
 print(data['MA5'])
